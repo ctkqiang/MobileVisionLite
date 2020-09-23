99325e493bda345d8fb54c156597ae8daacd31a9
John Melody Me <Johnmelodyme@yandex.com>
2020-09-23 10:33:44 +0800

    Init

--- app/src/main/java/com/johnmelodyme/tensorflowlite/VisionActivity.java
+++ app/src/main/java/com/johnmelodyme/tensorflowlite/VisionActivity.java
@@ -1,14 +1,22 @@
 package com.johnmelodyme.tensorflowlite;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class VisionActivity extends AppCompatActivity {
+    private static final String TAG = "VisionActivity";
+
+
+    private void ui_component() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ui_component();
     }
+
+
 }
